a1febf59ed469b0872345539f915ed8e43efb93e
formazione <gatto.gio@gmail.com>
2021-07-11 07:55:06 +0200

    new commit

--- backup/Crystals of Time 3.py
+++
@@ -1,3 +0,0 @@
-pygame 2.0.1 (SDL 2.0.14, Python 3.9.5)
-Hello from the pygame community. https://www.pygame.org/contribute.html
->>> 
--- backup/editor3b.py
+++
@@ -1,7 +0,0 @@
-pygame 2.0.1 (SDL 2.0.14, Python 3.9.5)
-Hello from the pygame community. https://www.pygame.org/contribute.html
-Traceback (most recent call last):
-  File "G:\timecrystals\editor3.py", line 76, in <module>
-    screen.blit(screen0, (0, 0))
-pygame.error: display Surface quit
->>> 
